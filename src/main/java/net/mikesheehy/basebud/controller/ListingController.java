97fdfdfb9db2770327f4793f8be3b0a7aefae951
Mike Sheehy <sheehy@outlook.com>
2024-12-28 21:21:50 -0500

    Update spring boot and controller

--- src/main/java/net/mikesheehy/basebud/controller/ListingController.java
+++ src/main/java/net/mikesheehy/basebud/controller/ListingController.java
@@ -30,37 +30,94 @@ public class ListingController {
   ListingRepository listingRepository;
 
   @GetMapping("/listings")
-  public ResponseEntity<List<Listing>> getAllTutorials(@RequestParam(required = false) String title) {
-    ...
-  }
+  public ResponseEntity<List<Listing>> getAllListings(@RequestParam(required = false) String title) {
+    try {
+        List<Listing> listings = new ArrayList<>();
 
-  @GetMapping("/listings/{id}")
-  public ResponseEntity<Listing> getTutorialById(@PathVariable("id") long id) {
-    ...
-  }
+        if (title == null)
+            listingRepository.findAll().forEach(listings::add);
+        else
+            listingRepository.findByTitleContaining(title).forEach(listings::add);
 
-  @PostMapping("/listings")
-  public ResponseEntity<Listing> createTutorial(@RequestBody Listing listing) {
-    ...
-  }
+        if (listings.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
 
-  @PutMapping("/listings/{id}")
-  public ResponseEntity<Listing> updateTutorial(@PathVariable("id") long id, @RequestBody Listing listing) {
-    ...
-  }
+        return new ResponseEntity<>(listings, HttpStatus.OK);
+    } catch (Exception e) {
+        return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
 
-  @DeleteMapping("/listings/{id}")
-  public ResponseEntity<HttpStatus> deleteTutorial(@PathVariable("id") long id) {
-    ...
-  }
+@GetMapping("/listings/{id}")
+public ResponseEntity<Listing> getListingById(@PathVariable("id") long id) {
+    Optional<Listing> listingData = listingRepository.findById(id);
 
-  @DeleteMapping("/listings")
-  public ResponseEntity<HttpStatus> deleteAllListings() {
-    ...
-  }
+    if (listingData.isPresent()) {
+        return new ResponseEntity<>(listingData.get(), HttpStatus.OK);
+    } else {
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
 
-  @GetMapping("/tutorials/published")
-  public ResponseEntity<List<Listing>> findByPublished() {
-    ...
-  }
+@PostMapping("/listings")
+public ResponseEntity<Listing> createListing(@RequestBody Listing listing) {
+    try {
+        Listing _listing = listingRepository
+                .save(new Listing(listing.getTitle(), listing.getDescription(), false));
+        return new ResponseEntity<>(_listing, HttpStatus.CREATED);
+    } catch (Exception e) {
+        return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
+
+@PutMapping("/listings/{id}")
+public ResponseEntity<Listing> updateListing(@PathVariable("id") long id, @RequestBody Listing listing) {
+    Optional<Listing> listingData = listingRepository.findById(id);
+
+    if (listingData.isPresent()) {
+        Listing _listing = listingData.get();
+        _listing.setTitle(listing.getTitle());
+        _listing.setDescription(listing.getDescription());
+        _listing.setPublished(listing.isPublished());
+        return new ResponseEntity<>(listingRepository.save(_listing), HttpStatus.OK);
+    } else {
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
+
+@DeleteMapping("/listings/{id}")
+public ResponseEntity<HttpStatus> deleteListing(@PathVariable("id") long id) {
+    try {
+        listingRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    } catch (Exception e) {
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
+
+@DeleteMapping("/listings")
+public ResponseEntity<HttpStatus> deleteAllListings() {
+    try {
+        listingRepository.deleteAll();
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    } catch (Exception e) {
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
+
+@GetMapping("/listings/published")
+public ResponseEntity<List<Listing>> findByPublished() {
+    try {
+        List<Listing> listings = listingRepository.findByPublished(true);
+
+        if (listings.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(listings, HttpStatus.OK);
+    } catch (Exception e) {
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
 }
